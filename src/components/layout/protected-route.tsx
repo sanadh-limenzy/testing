7176259d9b2e7fed70ae1f55afa98ca9ca05fefa
sanadh <sanadh.limenzy@gmail.com>
2025-09-29 08:59:32 +0530

    Add ProtectedRoute component for user authentication; update import case in AuthContext and remove console logs from PDFService

---
+++ src/components/layout/protected-route.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useAuthContext } from "@/contexts/AuthContext";
+import { usePathname, useRouter } from "next/navigation";
+import { SidebarProvider } from "../ui/sidebar";
+import { AppSidebar } from "./AppSidebar";
+import { Navbar } from "./Navbar";
+import { useEffect } from "react";
+import { useIsNotFoundPage } from "../providers/not-found-provider";
+
+const excludedRoutes = ["/auth", "/not-found", "/404"];
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ProtectedRoute({ children }: Props) {
+  const { user, loading } = useAuthContext();
+  const router = useRouter();
+  const pathname = usePathname();
+  const { isNotFoundPage } = useIsNotFoundPage();
+
+  useEffect(() => {
+    if (!user && !excludedRoutes.includes(pathname) && !loading) {
+      router.push("/auth");
+    }
+  }, [user, pathname, router, loading]);
+
+  if (!user && !excludedRoutes.includes(pathname)) {
+    return null;
+  }
+
+  if (excludedRoutes.includes(pathname) || isNotFoundPage) {
+    return <>{children}</>;
+  }
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="w-full">
+        <Navbar />
+        {children}
+      </div>
+    </SidebarProvider>
+  );
+}
